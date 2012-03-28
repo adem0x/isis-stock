2a7f0404773e2742d2f16eaba86ff625207aa551
federer1453@gmail.com <federer1453@gmail.com@9ff8e031-f3a0-498d-b22b-9e9a4baaef3d>
2012-03-28 19:17:14 +0000

    Adiçao dos arquivos csv Adiçao do formulário de edição

---
+++ trunk/isis-stock --username federer1453@gmail.com/unFrmEdit.pas
@@ -0,0 +1,24 @@
+unit unFrmEdit;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmEdit = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmEdit: TfrmEdit;
+
+implementation
+
+{$R *.dfm}
+
+end.
